49a75d8200ed56e38afa3ed66441ff331b06646d
fevic1 <fevic2@gmail.com>
2026-07-24 18:37:51 +0000

    feat(aios): connect event bus to automatic learning feedback

--- aios/learning/__init__.py
+++ aios/learning/__init__.py
@@ -1,10 +1,12 @@
 from .execution_memory import ExecutionMemory
 from .feedback import FeedbackAnalyzer
 from .coordinator import LearningCoordinator
+from .event_handler import LearningEventHandler
 
 
 __all__ = [
     "ExecutionMemory",
     "FeedbackAnalyzer",
     "LearningCoordinator",
+    "LearningEventHandler",
 ]
