62fac011b1d5a9935a5403914cabf7e650e99dac
fevic1 <fevic2@gmail.com>
2026-07-24 20:31:28 +0000

    fix(aios): repair runtime daemon indentation

---
+++ aios/runtime/daemon.py
@@ -0,0 +1,92 @@
+import time
+import logging
+
+from .config import RuntimeConfig
+
+
+logger = logging.getLogger(
+    "aios.runtime"
+)
+
+
+class AIOSRuntimeDaemon:
+
+
+    def __init__(
+        self,
+        supervisor,
+        config=None,
+    ):
+
+        self.supervisor = supervisor
+
+        self.config = (
+            config
+            or RuntimeConfig()
+        )
+
+        self.running = False
+
+
+
+    def run_once(
+        self,
+        projects,
+    ):
+
+        report = (
+            self.supervisor
+            .check_projects(
+                projects
+            )
+        )
+
+
+        if hasattr(
+            report,
+            "describe",
+        ):
+
+            output = report.describe()
+
+        else:
+
+            output = report
+
+
+        logger.info(
+            "Supervisor report: %s",
+            output,
+        )
+
+
+        return output
+
+
+
+    def start(
+        self,
+        projects,
+    ):
+
+        self.running = True
+
+
+        while self.running:
+
+            self.run_once(
+                projects
+            )
+
+
+            time.sleep(
+                self.config.interval_seconds
+            )
+
+
+
+    def stop(
+        self,
+    ):
+
+        self.running = False
