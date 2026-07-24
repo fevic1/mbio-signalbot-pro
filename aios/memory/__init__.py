e28e11af0519791039862b5dfd1030f70f0c58c9
fevic1 <fevic2@gmail.com>
2026-07-24 12:31:57 +0000

    feat(aios): add trading decision memory event helper

--- aios/memory/__init__.py
+++ aios/memory/__init__.py
@@ -3,6 +3,7 @@ from .indexer import MemoryIndexer
 from .obsidian import ObsidianWriter
 from .event_store import EventStore
 from .events import MemoryEvent
+from .decision import create_decision_event
 from .manager import MemoryManager
 from .models import MemoryEntry
 from .store import MemoryStore
@@ -14,4 +15,7 @@ __all__ = [
     "MemoryEntry",
     "MemoryStore",
     "MemoryIndexer",
+    "EventStore",
+    "MemoryEvent",
+    "create_decision_event",
 ]
