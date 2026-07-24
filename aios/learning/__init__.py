8663c7a5abd48d00bb6c4d8d7990c87f6a15dd64
fevic1 <fevic2@gmail.com>
2026-07-24 13:25:48 +0000

    feat(aios): add optimizer memory persistence

--- aios/learning/__init__.py
+++ aios/learning/__init__.py
@@ -2,10 +2,12 @@ from .feedback import ExecutionFeedback
 from .evaluator import ExecutionEvaluator
 from .optimizer import PlannerOptimizer
 from .feedback_store import FeedbackStore
+from .optimizer_store import OptimizerStore
 
 __all__ = [
     "ExecutionFeedback",
     "ExecutionEvaluator",
     "PlannerOptimizer",
     "FeedbackStore",
+    "OptimizerStore",
 ]
