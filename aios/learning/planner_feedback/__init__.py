23e55731140e66ee8791224505c4cbe71f7c6ea6
fevic1 <fevic2@gmail.com>
2026-07-24 20:12:31 +0000

    fix(aios): restore planner feedback engine compatibility

--- aios/learning/planner_feedback/__init__.py
+++ aios/learning/planner_feedback/__init__.py
@@ -1,8 +1,10 @@
 from .analyzer import PlannerLessonAnalyzer
 from .updater import PlannerFeedbackUpdater
+from .engine import PlannerFeedbackEngine
 
 
 __all__ = [
     "PlannerLessonAnalyzer",
     "PlannerFeedbackUpdater",
+    "PlannerFeedbackEngine",
 ]
