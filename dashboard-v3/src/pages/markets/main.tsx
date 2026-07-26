688130f8914a99bffc6ae42df77e15f12713643d
fevic1 <fevic2@gmail.com>
2026-07-26 02:41:31 +0000

    feat(frontend): initialize multi page dashboard architecture

---
+++ dashboard-v3/src/pages/markets/main.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import "@/index.css";
+
+function App() {
+  return (
+    <div>
+      <h1>MBIO SignalPro markets</h1>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(<App />);
