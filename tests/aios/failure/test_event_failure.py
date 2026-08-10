b5041e40e4ca66a97d3168f2d20f80ab6463a701
fevic1 <fevic2@gmail.com>
2026-08-10 20:53:29 +0700

    chore: remove retired AIOS test tree

--- tests/aios/failure/test_event_failure.py
+++
@@ -1,8 +0,0 @@
-def test_event_failure_isolated():
-
-    event_result = {
-        "published": False,
-        "error": "event bus unavailable",
-    }
-
-    assert event_result["published"] is False
